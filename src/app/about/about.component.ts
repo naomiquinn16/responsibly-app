5b084971e08910f90527609b21f49fbfe39eb2a3
Quinn <naomi.e.quinn@accenture.com>
2020-02-07 01:51:40 +0000

    team section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,7 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
+  members = [
+    {
+      name: 'Abhishek Khanna',
+      image: '../../assets/images/boy.jpeg',
+      role: 'Co Founder & CEO',
+      linkedin: 'https://ie.linkedin.com/',
+      twitter: 'https://twitter.com',
+      github: 'https://github.com',
+    },
+    {
+      name: 'Naomi Quinn',
+      image: '../../assets/images/girl.jpeg',
+      role: 'Co Founder & COO',
+      linkedin: 'https://ie.linkedin.com/',
+      twitter: 'https://twitter.com',
+      github: 'https://github.com',
+    },
+    {
+      name: 'New Person',
+      image: '../../assets/images/woman.jpg',
+      role: 'Co Founder & CFO',
+      linkedin: 'https://ie.linkedin.com/',
+      twitter: 'https://twitter.com',
+      github: 'https://github.com',
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
