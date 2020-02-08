152d54d387da411011498b432d49f4547f4d099b
Quinn <naomi.e.quinn@accenture.com>
2020-02-08 23:49:22 +0000

    product background

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
-
+  stats = [
+    {
+      value: '$15.7 tr',
+      text: 'Potential contribution to the global economy by 2030 from AI'
+    },
+    {
+      value: '+ 26%',
+      text: 'Up to 26% boost in GDP for local economies from AI by 2030'
+    },
+    {
+      value: '+ 80%',
+      text:  '80% of customer interactions will be done without humans in 2020'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
