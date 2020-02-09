43812224c963be33d883f8acdf70df2d730e2ed0
Quinn <naomi.e.quinn@accenture.com>
2020-02-09 00:36:13 +0000

    integ and footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  icons = [
+    {
+      name: 'LinkedIn',
+      url: 'https://ie.linkedin.com/',
+      path: '../../assets/images/linkedin-in-brands.svg'
+    },
+    {
+      name: 'Twitter',
+      twitter: 'https://twitter.com',
+      path: '../../assets/images/twitter-brands.svg'
+    },
+    {
+      name: 'Github',
+      github: 'https://github.com',
+      path: '../../assets/images/github-brands.svg'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
